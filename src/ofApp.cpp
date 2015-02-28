d2827256712b1a52ef6c8a21ab161d2c9abe227e
GillyTheFish <PGilbertSchmitt@gmail.com>
2015-02-28 15:11:25 -0800

    clearer response

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    Seat first;
-    Seat second;
-    Seat third;
+    Seat first; //Creating the first seat
+    Seat second; //Creating the second seat
+    Seat third; //Creating the third seat
 
-    cout << first.getDataName() << second.getDataName() << third.getDataName() << endl;
+    /*This is a test to show that each seat has its own filename for its respective xml file*/
+    cout << "The file for seat " << first.getSeatNum() << " is called " << first.getDataName() << endl;
+    cout << "The file for seat " << second.getSeatNum() << " is called " << second.getDataName() << endl;
+    cout << "The file for seat " << third.getSeatNum() << " is called " << third.getDataName() << endl;
 }
 
 //--------------------------------------------------------------
